d53fed3fe08f36800129b906fec6cbf8438c7b18
dangchienhsgs <dangchienhsgs@gmail.com>
2015-09-10 16:05:35 +0700

    Add fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+alias cdproject="cd /mnt/Data/Projects/"
+alias cdlearning="cd /mnt/Data/Learning/"
+alias cdresources="cd /mnt/Data/Tai-lieu/"
+alias cddata="cd /mnt/Data/"
+alias cdsetup="cd /mnt/Setup/"
+
+set -x SCALA_HOME '/usr/local/share/scala'
+set -x PATH $SCALA_HOME/bin $PATH
+
+
