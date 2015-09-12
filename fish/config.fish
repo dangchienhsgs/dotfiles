cb89a8ca8752ee624c48f50731d4ebf872fc81d9
dangchienhsgs <dangchienhsgs@gmail.com>
2015-09-12 17:25:44 +0700

    Add gopath

--- fish/config.fish
+++ fish/config.fish
@@ -6,5 +6,7 @@ alias cdsetup="cd /mnt/Setup/"
 
 set -x SCALA_HOME '/usr/local/share/scala'
 set -x PATH $SCALA_HOME/bin $PATH
-
-
+set -x ACTIVATOR '/home/supercoder/activator/'
+set -x PATH $ACTIVATOR $PATH
+set -x GOPATH '~/.go'
+set -x GOROOT '/usr/lib/go'
