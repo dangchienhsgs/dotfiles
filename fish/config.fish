69ad7cfe153825c512a2e7b494b2bc77cf88c830
Chien D Nguyen <dangchienhsgs@gmail.com>
2016-03-31 15:24:39 +0700

    Relaod

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,5 @@ set -x SCALA_HOME '/usr/local/share/scala'
 set -x PATH $SCALA_HOME/bin $PATH
 set -x ACTIVATOR '/home/supercoder/activator/'
 set -x PATH $ACTIVATOR $PATH
-set -x GOPATH '~/.go'
+set -x GOPATH '/mnt/Data/Projects/go'
 set -x GOROOT '/usr/lib/go'
